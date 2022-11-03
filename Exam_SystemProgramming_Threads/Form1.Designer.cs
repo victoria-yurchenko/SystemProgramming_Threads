dbc2271908d6fc57f9b2f102ca23ba3b1d1c0b08
ACER <ACER@DESKTOP-GDVFM0R>
2022-11-03 19:12:14 +0200

    Add project files.

---
+++ Exam_SystemProgramming_Threads/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Exam_SystemProgramming_Threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nudEnterField = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudEnterField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudEnterField
+            // 
+            this.nudEnterField.BackColor = System.Drawing.Color.DimGray;
+            this.nudEnterField.ForeColor = System.Drawing.Color.GreenYellow;
+            this.nudEnterField.Location = new System.Drawing.Point(12, 411);
+            this.nudEnterField.Name = "nudEnterField";
+            this.nudEnterField.Size = new System.Drawing.Size(123, 27);
+            this.nudEnterField.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.DimGray;
+            this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnStart.Font = new System.Drawing.Font("Sitka Heading", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.ForeColor = System.Drawing.Color.GreenYellow;
+            this.btnStart.Location = new System.Drawing.Point(141, 409);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 29);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(648, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.nudEnterField);
+            this.Name = "Form1";
+            this.Text = "Exam";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.nudEnterField)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudEnterField;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ Exam_SystemProgramming_Threads/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Exam_SystemProgramming_Threads
+{
+    public delegate void HelperToCall(ProgressBar progressBar);
+
+    public partial class Form1 : Form
+    {
+        private const int _COUNT_THREADS_SIMULTANEOUSLY = 3;
+        private ICollection<ProgressBar> _progressBars = null;
+        private ICollection<Label> _labels = null;
+        private Random _random = null;
+        private Semaphore _semaphore = null;
+        private HelperToCall _helperToCall = null;
+        private int _positionInList = 0;
+        private bool _isRunningProcess = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            nudEnterField.Maximum = 10;
+            btnStart.Enabled = false;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            timer.Start();
+
+            _random = new Random();
+            _progressBars = new List<ProgressBar>();
+            _labels = new List<Label>();
+            _helperToCall = new HelperToCall(ChangeValue);
+        }
+
+        private void DeleteFullProgressBars()
+        {
+            foreach (var progressBar in _progressBars)
+            {
+                if (progressBar.Value == progressBar.Maximum)
+                {
+                    var label = _labels.Where
+                    (l =>
+                        l.Location.X == progressBar.Location.X + progressBar.Width &&
+                        l.Location.Y == progressBar.Location.Y
+                    ).FirstOrDefault();
+
+                    _labels.Remove(label);
+                    _progressBars.Remove(progressBar);
+                    Controls.Remove(label);
+                    Controls.Remove(progressBar);
+                    _positionInList--;
+                    break;
+                }
+            }
+        }
+
+        private ProgressBar GetNextProgressBar()
+        {
+            int i = 0;
+            foreach (var progressBar in _progressBars)
+            {
+                if (i == _positionInList)
+                {
+                    _positionInList++;
+                    return progressBar;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return null;
+        }
+
+        private void ChangeValue(ProgressBar progressBar)
+        {
+            if (progressBar.Value != progressBar.Maximum)
+            {
+                progressBar.Value++;
+            }
+        }
+
+        private void ExecuteProgressBar()
+        {
+            _semaphore.WaitOne();
+
+            var progressBar = GetNextProgressBar();
+
+            for (int i = 0; i < progressBar?.Maximum; i++)
+            {
+                Thread.Sleep(1000);
+                Invoke(_helperToCall, progressBar);
+            }
+            _semaphore.Release();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_progressBars.Count < 10)
+            {
+                try
+                {
+                    int number = _random.Next(1, Convert.ToInt32(nudEnterField.Value));
+
+                    ProgressBar progressBar = new ProgressBar();
+                    progressBar.Location = new Point(e.X, e.Y);
+                    progressBar.Width = 250;
+                    progressBar.Height = 20;
+                    progressBar.Maximum = number;
+                    progressBar.Minimum = 0;
+
+                    Label label = new Label();
+                    label.Location = new Point(e.X + progressBar.Width, e.Y);
+                    label.Text = number.ToString();
+                    label.ForeColor = Color.YellowGreen;
+
+                    this.Controls.Add(progressBar);
+                    this.Controls.Add(label);
+                    _progressBars.Add(progressBar);
+                    _labels.Add(label);
+                }
+                catch
+                {
+                    MessageBox.Show("Cannot create progress bar without entered " +
+                        "value in the numeric up and down field!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("The number of progress bars cannot be " +
+                    "more than 10!");
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            _isRunningProcess = true;
+            nudEnterField.Value = 0;
+
+            if (_semaphore == null)
+            {
+                _semaphore = new Semaphore(_COUNT_THREADS_SIMULTANEOUSLY, _COUNT_THREADS_SIMULTANEOUSLY);
+            }
+
+            for (int i = 0; i < _progressBars.Count; i++)
+            {
+                var thread = new Thread(ExecuteProgressBar);
+                thread.IsBackground = true;
+                thread.Start();
+            }
+        }
+
+        private void UnrunningProcess()
+        {
+            if (_progressBars.Count == 0)
+            {
+                _isRunningProcess = false;
+            }
+        }
+
+        private void ValidateNumericUpDown()
+        {
+            if (!_isRunningProcess)
+            {
+                nudEnterField.Enabled = true;
+            }
+            else
+            {
+                nudEnterField.Enabled = false;
+            }
+        }
+
+        private void ValidateButtonStart()
+        {
+            if (nudEnterField.Value > 0 &&
+                _progressBars.Count > 0 &&
+                !_isRunningProcess)
+            {
+                btnStart.Enabled = true;
+            }
+            else
+            {
+                btnStart.Enabled = false;
+            }
+        }
+
+        private void ValidateControls()
+        {
+            UnrunningProcess();
+            ValidateNumericUpDown();
+            ValidateButtonStart();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ValidateControls();
+            DeleteFullProgressBars();
+            this.Refresh();
+        }
+    }
+}
